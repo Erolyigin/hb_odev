939f9e92ef16a2cce8ec2066d5ef8f3dc36a22ef
muhammeterol.yigin@halkbank.com.tr <muhammeterol.yigin@halkbank.com.tr>
2022-08-28 23:56:20 +0300

    "hbodev yayınlandı"

---
+++ HalkBankProjeOdevi/HalkBankListDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using HalkBankCore;
+
+namespace HalkBankListDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new HalkBankList();
+            list.Ekle("Salih");
+            list.Ekle("Engin");
+            list.Ekle("Mustafa");
+            list.Ekle("Ahmet");
+            list.Ekle("Erol");
+
+            list[2] = "Mehmet";
+
+            list.Sil(3);
+            list.KosulluSil(p => p.Equals("Erol"));
+
+            Console.WriteLine("Eleman Sayısı:{0}", list.ElemanSayisi);
+            foreach (var eleman in list)
+            {
+                Console.WriteLine(eleman);
+            }
+
+            list.Temizle();
+
+            Console.WriteLine("Eleman Sayısı:{0}", list.ElemanSayisi);
+
+            Console.ReadLine();
+        }
+    }
+}
